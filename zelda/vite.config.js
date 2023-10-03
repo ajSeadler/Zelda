2f39225ead9ae6d32bb45ddfd4bf265705a5b79e
ajSeadler <aj6287@gmail.com>
2023-10-03 09:48:39 -0500

    routes

--- zelda/vite.config.js
+++ zelda/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Specify the port for your development server
+  },
+  resolve: {
+    alias: {
+      // Define custom aliases for importing modules
+      '@components': '/src/components',
+      '@styles': '/src/styles',
+    },
+  },
+  build: {
+    outDir: 'dist', // Specify the output directory for production builds
+  },
+});
